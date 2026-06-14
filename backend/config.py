cce31c6628cfd1585ce89d931093a90903d36f6c
Rufai Sodiq Sijuade <sodiqrufai75@gmail.com>
2026-06-14 14:18:22 +0100

    Add Render deployment config

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,12 @@
 import os
 
 class Config:
-    DB_HOST = "localhost"
-    DB_PORT = "5432"
-    DB_NAME = "gis_dss"
-    DB_USER = "postgres"
-    DB_PASSWORD = "sijuade2004"  # your actual password
+    DB_HOST     = os.getenv("DB_HOST", "localhost")
+    DB_PORT     = os.getenv("DB_PORT", "5432")
+    DB_NAME     = os.getenv("DB_NAME", "postgres")
+    DB_USER     = os.getenv("DB_USER", "postgres")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
